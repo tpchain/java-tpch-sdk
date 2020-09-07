e198a7d486d6675939c79a46b169d3bb8d4b541b
zzx <slightingery@163.COM>
2020-09-07 16:41:03 +0800

    test

--- abi/src/main/java/org/tpc/abi/DefaultFunctionEncoder.java
+++ abi/src/main/java/org/tpc/abi/DefaultFunctionEncoder.java
@@ -10,15 +10,15 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.abi;
+package org.tpc.abi;
 
 import java.math.BigInteger;
 import java.util.List;
 
-import org.web3j.abi.datatypes.Function;
-import org.web3j.abi.datatypes.StaticArray;
-import org.web3j.abi.datatypes.Type;
-import org.web3j.abi.datatypes.Uint;
+import org.tpc.abi.datatypes.Function;
+import org.tpc.abi.datatypes.StaticArray;
+import org.tpc.abi.datatypes.Type;
+import org.tpc.abi.datatypes.Uint;
 
 public class DefaultFunctionEncoder extends FunctionEncoder {
 
