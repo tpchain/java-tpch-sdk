9ac8e427a12727307a708c61eb4ffaa9b2d2fed3
zzx <slightingery@163.COM>
2020-09-07 16:22:06 +0800

    test

--- abi/src/main/java/org/tpc/abi/datatypes/Bytes.java
+++ abi/src/main/java/org/tpc/abi/datatypes/Bytes.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright 2019 Web3 Labs Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.abi.datatypes;
+package org.tpc.abi.datatypes;
 
 /** Statically allocated sequence of bytes. */
 public class Bytes extends BytesType {
