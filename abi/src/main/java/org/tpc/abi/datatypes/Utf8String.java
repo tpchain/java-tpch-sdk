f0bb24dc07f55fef5950b684075b5a932ed98d16
zzx <slightingery@163.COM>
2020-09-07 16:38:11 +0800

    test

--- abi/src/main/java/org/tpc/abi/datatypes/Utf8String.java
+++ abi/src/main/java/org/tpc/abi/datatypes/Utf8String.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.abi.datatypes;
+package org.tpc.abi.datatypes;
 
 /** UTF-8 encoded string type. */
 public class Utf8String implements Type<String> {
