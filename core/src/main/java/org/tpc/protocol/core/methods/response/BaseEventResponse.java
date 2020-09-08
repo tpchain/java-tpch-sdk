2e46711e1646855108bdded9a2a3be19a84e8ec0
zzx <slightingery@163.COM>
2020-09-08 17:13:39 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/methods/response/BaseEventResponse.java
+++ core/src/main/java/org/tpc/protocol/core/methods/response/BaseEventResponse.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.methods.response;
+package org.tpc.protocol.core.methods.response;
 
 /** Base-class of EventResponse objects. Carries the log object, shared by all event responses */
 public class BaseEventResponse {
