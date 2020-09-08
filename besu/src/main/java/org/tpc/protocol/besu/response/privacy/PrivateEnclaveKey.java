9aa80479b6f1be471ead413fb6c8cd0e06115415
zzx <slightingery@163.COM>
2020-09-08 09:44:15 +0800

    test

--- besu/src/main/java/org/tpc/protocol/besu/response/privacy/PrivateEnclaveKey.java
+++ besu/src/main/java/org/tpc/protocol/besu/response/privacy/PrivateEnclaveKey.java
@@ -10,9 +10,9 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.besu.response.privacy;
+package org.tpc.protocol.besu.response.privacy;
 
-import org.web3j.protocol.core.Response;
+import org.tpc.protocol.core.Response;
 
 public class PrivateEnclaveKey extends Response<String> {
     public String getKey() {
