7dab18e7a224a0fb27bd76496752d2f70299092a
zzx <slightingery@163.COM>
2020-09-08 15:04:22 +0800

    test

--- core/src/main/java/org/tpc/contracts/token/ERC20BasicInterface.java
+++ core/src/main/java/org/tpc/contracts/token/ERC20BasicInterface.java
@@ -10,16 +10,16 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.contracts.token;
+package org.tpc.contracts.token;
 
 import java.math.BigInteger;
 import java.util.List;
 
 import io.reactivex.Flowable;
 
-import org.web3j.protocol.core.DefaultBlockParameter;
-import org.web3j.protocol.core.RemoteCall;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.tpc.protocol.core.DefaultBlockParameter;
+import org.tpc.protocol.core.RemoteCall;
+import org.tpc.protocol.core.methods.response.TransactionReceipt;
 
 /**
  * Describes the Ethereum "Basic" subset of the ERC-20 token standard.
