3e39f1edc8d53025e9027678d15f0b7783e4c13d
zzx <slightingery@163.COM>
2020-09-08 11:52:22 +0800

    test

--- codegen/src/test/java/org/tpc/codegen/unit/gen/kotlin/KotlinClassGeneratorTest.java
+++ codegen/src/test/java/org/tpc/codegen/unit/gen/kotlin/KotlinClassGeneratorTest.java
@@ -10,13 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.codegen.unit.gen.kotlin;
+package org.tpc.codegen.unit.gen.kotlin;
 
 import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
-import org.web3j.codegen.unit.gen.java.JavaClassGenerator;
+import org.tpc.codegen.unit.gen.java.JavaClassGenerator;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
