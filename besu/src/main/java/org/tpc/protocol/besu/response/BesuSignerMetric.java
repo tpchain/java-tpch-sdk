bdaf5bd3d0da25e39388ebe0d68a99e23f57e07e
zzx <slightingery@163.COM>
2020-09-08 09:36:17 +0800

    test

--- besu/src/main/java/org/tpc/protocol/besu/response/BesuSignerMetric.java
+++ besu/src/main/java/org/tpc/protocol/besu/response/BesuSignerMetric.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.besu.response;
+package org.tpc.protocol.besu.response;
 
 import java.math.BigInteger;
 import java.util.Objects;
@@ -18,7 +18,7 @@ import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 public class BesuSignerMetric {
 
