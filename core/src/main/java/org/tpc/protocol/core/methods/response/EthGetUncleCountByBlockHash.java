59f2a874aa1434b06c7a9f37a9b70468bd427368
zzx <slightingery@163.COM>
2020-09-08 17:43:02 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/methods/response/EthGetUncleCountByBlockHash.java
+++ core/src/main/java/org/tpc/protocol/core/methods/response/EthGetUncleCountByBlockHash.java
@@ -10,12 +10,12 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.methods.response;
+package org.tpc.protocol.core.methods.response;
 
 import java.math.BigInteger;
 
-import org.web3j.protocol.core.Response;
-import org.web3j.utils.Numeric;
+import org.tpc.protocol.core.Response;
+import org.tpc.utils.Numeric;
 
 /** eth_getUncleCountByBlockHash. */
 public class EthGetUncleCountByBlockHash extends Response<String> {
