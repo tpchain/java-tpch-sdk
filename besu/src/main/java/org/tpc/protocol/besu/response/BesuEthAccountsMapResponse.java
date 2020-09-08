74c8661b6fe3d642bd09e2a815abf7e479c43117
zzx <slightingery@163.COM>
2020-09-08 09:32:26 +0800

    test

--- besu/src/main/java/org/tpc/protocol/besu/response/BesuEthAccountsMapResponse.java
+++ besu/src/main/java/org/tpc/protocol/besu/response/BesuEthAccountsMapResponse.java
@@ -10,11 +10,11 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.besu.response;
+package org.tpc.protocol.besu.response;
 
 import java.util.Map;
 
-import org.web3j.protocol.core.Response;
+import org.tpc.protocol.core.Response;
 
 public class BesuEthAccountsMapResponse extends Response<Map<String, Boolean>> {
     public Map<String, Boolean> getAccounts() {
