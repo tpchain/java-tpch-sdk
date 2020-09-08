f9372d12b4d74e0a6307ab4b0c95b8180ced7d39
zzx <slightingery@163.COM>
2020-09-08 17:39:35 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/methods/response/EthGetStorageAt.java
+++ core/src/main/java/org/tpc/protocol/core/methods/response/EthGetStorageAt.java
@@ -10,9 +10,9 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.methods.response;
+package org.tpc.protocol.core.methods.response;
 
-import org.web3j.protocol.core.Response;
+import org.tpc.protocol.core.Response;
 
 /** eth_getStorageAt. */
 public class EthGetStorageAt extends Response<String> {
