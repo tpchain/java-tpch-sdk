63d429969d21ba32dc2ef74d5cdbf956d14b67ff
zzx <slightingery@163.COM>
2020-09-08 17:00:26 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/filters/PendingTransactionsFilter.java
+++ core/src/main/java/org/tpc/protocol/core/filters/PendingTransactionsFilter.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.filters;
+package org.tpc.protocol.core.filters;
 
 import java.io.IOException;
 import java.math.BigInteger;
@@ -18,10 +18,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.Request;
-import org.web3j.protocol.core.methods.response.EthFilter;
-import org.web3j.protocol.core.methods.response.EthLog;
+import org.tpc.protocol.Web3j;
+import org.tpc.protocol.core.Request;
+import org.tpc.protocol.core.methods.response.EthFilter;
+import org.tpc.protocol.core.methods.response.EthLog;
 
 /** Handler hashes for working with transaction filter requests. */
 public class PendingTransactionsFilter extends Filter<List<String>> {
