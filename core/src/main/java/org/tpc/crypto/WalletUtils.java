c5c192fa1287bd0ab12f41ecdd68e4e34bf932b9
zzx <slightingery@163.COM>
2020-09-08 15:11:57 +0800

    test

--- core/src/main/java/org/tpc/crypto/WalletUtils.java
+++ core/src/main/java/org/tpc/crypto/WalletUtils.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,11 +26,11 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
-import static org.web3j.crypto.Hash.sha256;
-import static org.web3j.crypto.Keys.ADDRESS_LENGTH_IN_HEX;
-import static org.web3j.crypto.Keys.PRIVATE_KEY_LENGTH_IN_HEX;
+import static org.tpc.crypto.Hash.sha256;
+import static org.tpc.crypto.Keys.ADDRESS_LENGTH_IN_HEX;
+import static org.tpc.crypto.Keys.PRIVATE_KEY_LENGTH_IN_HEX;
 
 /** Utility functions for working with Wallet files. */
 public class WalletUtils {
