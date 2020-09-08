ac7fbb26cc1a523509b364afea51d19b15f99839
zzx <slightingery@163.COM>
2020-09-08 16:46:25 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/filters/Callback.java
+++ core/src/main/java/org/tpc/protocol/core/filters/Callback.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.filters;
+package org.tpc.protocol.core.filters;
 
 /** Filter callback interface. */
 public interface Callback<T> {
