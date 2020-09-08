0e030c66f5bf1025c4bca7bb538eb58bfb7941d1
zzx <slightingery@163.COM>
2020-09-08 16:51:57 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/filters/FilterException.java
+++ core/src/main/java/org/tpc/protocol/core/filters/FilterException.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.filters;
+package org.tpc.protocol.core.filters;
 
 /** Filter exception wrapper. */
 public class FilterException extends RuntimeException {
