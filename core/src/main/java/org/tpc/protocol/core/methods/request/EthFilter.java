4befac7152e565b3d9c8a9bcb0990d33e40d40ed
zzx <slightingery@163.COM>
2020-09-08 17:02:53 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/methods/request/EthFilter.java
+++ core/src/main/java/org/tpc/protocol/core/methods/request/EthFilter.java
@@ -10,12 +10,12 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.methods.request;
+package org.tpc.protocol.core.methods.request;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.web3j.protocol.core.DefaultBlockParameter;
+import org.tpc.protocol.core.DefaultBlockParameter;
 
 /**
  * Filter implementation as per <a
