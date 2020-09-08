e6a3960e41970d474923a902173d998e614374af
zzx <slightingery@163.COM>
2020-09-08 10:05:31 +0800

    test

--- besu/src/main/java/org/tpc/utils/PrivacyGroupUtils.java
+++ besu/src/main/java/org/tpc/utils/PrivacyGroupUtils.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.utils;
+package org.tpc.utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,11 +19,11 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.web3j.crypto.Hash;
-import org.web3j.rlp.RlpEncoder;
-import org.web3j.rlp.RlpList;
-import org.web3j.rlp.RlpString;
-import org.web3j.rlp.RlpType;
+import org.tpc.crypto.Hash;
+import org.tpc.rlp.RlpEncoder;
+import org.tpc.rlp.RlpList;
+import org.tpc.rlp.RlpString;
+import org.tpc.rlp.RlpType;
 
 public class PrivacyGroupUtils {
     public static Base64String generateLegacyGroup(
