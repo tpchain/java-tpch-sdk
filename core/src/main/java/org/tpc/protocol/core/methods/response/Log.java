fe0321a5e4a05467c8506c18f38d79280b79a69e
zzx <slightingery@163.COM>
2020-09-08 18:00:46 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/methods/response/Log.java
+++ core/src/main/java/org/tpc/protocol/core/methods/response/Log.java
@@ -10,12 +10,12 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.methods.response;
+package org.tpc.protocol.core.methods.response;
 
 import java.math.BigInteger;
 import java.util.List;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 /**
  * Log object used by {@link EthLog} and {@link EthGetTransactionReceipt}.
