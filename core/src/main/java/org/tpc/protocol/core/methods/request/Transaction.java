10716a5939e6409fca92b16d5c5484020853de90
zzx <slightingery@163.COM>
2020-09-08 17:07:03 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/methods/request/Transaction.java
+++ core/src/main/java/org/tpc/protocol/core/methods/request/Transaction.java
@@ -10,13 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.methods.request;
+package org.tpc.protocol.core.methods.request;
 
 import java.math.BigInteger;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 /**
  * Transaction request object used the below methods.
