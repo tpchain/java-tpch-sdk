98ba1e05eb3a2d4d9d871716d668e21e512ff662
zzx <slightingery@163.COM>
2020-09-08 17:06:11 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/methods/request/ShhPost.java
+++ core/src/main/java/org/tpc/protocol/core/methods/request/ShhPost.java
@@ -10,14 +10,14 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.methods.request;
+package org.tpc.protocol.core.methods.request;
 
 import java.math.BigInteger;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 /** https://github.com/ethereum/wiki/wiki/JSON-RPC#shh_post */
 @JsonInclude(JsonInclude.Include.NON_NULL)
