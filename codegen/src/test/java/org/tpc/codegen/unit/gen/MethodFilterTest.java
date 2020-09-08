b5763e7e29726cc5bd7e9998bbcbc36a6488add0
zzx <slightingery@163.COM>
2020-09-08 11:39:22 +0800

    test

--- codegen/src/test/java/org/tpc/codegen/unit/gen/MethodFilterTest.java
+++ codegen/src/test/java/org/tpc/codegen/unit/gen/MethodFilterTest.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.codegen.unit.gen;
+package org.tpc.codegen.unit.gen;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -19,10 +19,10 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import org.web3j.codegen.unit.gen.java.Setup;
-import org.web3j.protocol.Web3j;
-import org.web3j.tx.TransactionManager;
-import org.web3j.tx.gas.ContractGasProvider;
+import org.tpc.codegen.unit.gen.java.Setup;
+import org.tpc.protocol.Web3j;
+import org.tpc.tx.TransactionManager;
+import org.tpc.tx.gas.ContractGasProvider;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
