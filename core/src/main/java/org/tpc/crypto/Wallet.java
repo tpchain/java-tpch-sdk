82a825b6eb64b6d41664cdd4c1dc86e4e7e08401
zzx <slightingery@163.COM>
2020-09-08 15:09:36 +0800

    test

--- core/src/main/java/org/tpc/crypto/Wallet.java
+++ core/src/main/java/org/tpc/crypto/Wallet.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -29,10 +29,10 @@ import org.bouncycastle.crypto.generators.PKCS5S2ParametersGenerator;
 import org.bouncycastle.crypto.generators.SCrypt;
 import org.bouncycastle.crypto.params.KeyParameter;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.web3j.crypto.SecureRandomUtils.secureRandom;
+import static org.tpc.crypto.SecureRandomUtils.secureRandom;
 
 /**
  * Ethereum wallet file management. For reference, refer to <a
