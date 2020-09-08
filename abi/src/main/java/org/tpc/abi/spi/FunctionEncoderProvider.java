d39286da47b32dd6f749a297b60204c1aa7a2a10
zzx <slightingery@163.COM>
2020-09-08 09:01:39 +0800

    test

--- abi/src/main/java/org/tpc/abi/spi/FunctionEncoderProvider.java
+++ abi/src/main/java/org/tpc/abi/spi/FunctionEncoderProvider.java
@@ -10,11 +10,11 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.abi.spi;
+package org.tpc.abi.spi;
 
 import java.util.function.Supplier;
 
-import org.web3j.abi.FunctionEncoder;
+import org.tpc.abi.FunctionEncoder;
 
 /** Function encoding Service Provider Interface. */
 public interface FunctionEncoderProvider extends Supplier<FunctionEncoder> {}
