b99b24dab89cbbdf6416aa3eacf75533caf4a40a
zzx <slightingery@163.COM>
2020-09-09 16:19:14 +0800

    test

--- integration-tests/src/test/java/org/tpc/ens/EnsIT.java
+++ integration-tests/src/test/java/org/tpc/ens/EnsIT.java
@@ -10,12 +10,12 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.ens;
+package org.tpc.ens;
 
 import org.junit.jupiter.api.Test;
 
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.http.HttpService;
+import org.tpc.protocol.Web3j;
+import org.tpc.protocol.http.HttpService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
