477862b054c83fe68830f1356e48a55750c0d87e
zzx <slightingery@163.COM>
2020-09-09 15:14:15 +0800

    test

--- crypto/src/test/java/org/tpc/crypto/SignTest.java
+++ crypto/src/test/java/org/tpc/crypto/SignTest.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import java.math.BigInteger;
 import java.security.SignatureException;
@@ -18,7 +18,7 @@ import java.security.SignatureException;
 import org.bouncycastle.math.ec.ECPoint;
 import org.junit.jupiter.api.Test;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
