b29992966db0646f09526e4e12902a6613cb0b43
zzx <slightingery@163.COM>
2020-09-09 14:42:49 +0800

    test

--- crypto/src/main/java/org/tpc/crypto/ECKeyPair.java
+++ crypto/src/main/java/org/tpc/crypto/ECKeyPair.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import java.math.BigInteger;
 import java.security.KeyPair;
@@ -23,7 +23,7 @@ import org.bouncycastle.crypto.signers.HMacDSAKCalculator;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 /** Elliptic Curve SECP-256k1 generated key pair. */
 public class ECKeyPair {
