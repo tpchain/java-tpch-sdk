aba7eb3c24ddea938b75bbe1ea295a82284c29f1
zzx <slightingery@163.COM>
2020-09-09 15:09:13 +0800

    test

--- crypto/src/test/java/org/tpc/crypto/KeysTest.java
+++ crypto/src/test/java/org/tpc/crypto/KeysTest.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import java.math.BigInteger;
 import java.security.KeyPair;
@@ -20,8 +20,8 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
-import org.web3j.utils.Numeric;
-import org.web3j.utils.Strings;
+import org.tpc.utils.Numeric;
+import org.tpc.utils.Strings;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
