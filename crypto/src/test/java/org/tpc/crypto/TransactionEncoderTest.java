33bc75b7b1bfddedad38de8c821d43a711df72d3
zzx <slightingery@163.COM>
2020-09-09 15:19:12 +0800

    test

--- crypto/src/test/java/org/tpc/crypto/TransactionEncoderTest.java
+++ crypto/src/test/java/org/tpc/crypto/TransactionEncoderTest.java
@@ -10,16 +10,16 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import java.math.BigInteger;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import org.web3j.rlp.RlpString;
-import org.web3j.rlp.RlpType;
-import org.web3j.utils.Numeric;
+import org.tpc.rlp.RlpString;
+import org.tpc.rlp.RlpType;
+import org.tpc.utils.Numeric;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
