59017c9041984a4832fc9b28657b8fcefc9a88c0
zzx <slightingery@163.COM>
2020-09-09 14:24:03 +0800

    test

--- core/src/test/java/org/tpc/tx/ReadonlyTransactionManagerTest.java
+++ core/src/test/java/org/tpc/tx/ReadonlyTransactionManagerTest.java
@@ -10,25 +10,25 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.tx;
+package org.tpc.tx;
 
 import java.io.IOException;
 import java.math.BigInteger;
 
 import org.junit.jupiter.api.Test;
 
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.Web3jService;
-import org.web3j.protocol.core.DefaultBlockParameter;
-import org.web3j.protocol.core.methods.response.EthCall;
-import org.web3j.tx.exceptions.ContractCallException;
+import org.tpc.protocol.Web3j;
+import org.tpc.protocol.Web3jService;
+import org.tpc.protocol.core.DefaultBlockParameter;
+import org.tpc.protocol.core.methods.response.EthCall;
+import org.tpc.tx.exceptions.ContractCallException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.web3j.tx.TransactionManager.REVERT_ERR_STR;
+import static org.tpc.tx.TransactionManager.REVERT_ERR_STR;
 
 public class ReadonlyTransactionManagerTest {
 
