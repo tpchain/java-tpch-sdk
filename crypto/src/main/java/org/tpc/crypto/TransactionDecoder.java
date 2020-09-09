6d4d1a05d2b6d986553b37231c8eee9a5a0861eb
zzx <slightingery@163.COM>
2020-09-09 14:57:35 +0800

    test

--- crypto/src/main/java/org/tpc/crypto/TransactionDecoder.java
+++ crypto/src/main/java/org/tpc/crypto/TransactionDecoder.java
@@ -10,14 +10,14 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import java.math.BigInteger;
 
-import org.web3j.rlp.RlpDecoder;
-import org.web3j.rlp.RlpList;
-import org.web3j.rlp.RlpString;
-import org.web3j.utils.Numeric;
+import org.tpc.rlp.RlpDecoder;
+import org.tpc.rlp.RlpList;
+import org.tpc.rlp.RlpString;
+import org.tpc.utils.Numeric;
 
 public class TransactionDecoder {
 
