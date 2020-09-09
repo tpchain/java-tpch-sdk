43b77a4dc0880d67a8b37eb761541782baccef4c
zzx <slightingery@163.COM>
2020-09-09 10:15:50 +0800

    test

--- core/src/main/java/org/tpc/tx/FastRawTransactionManager.java
+++ core/src/main/java/org/tpc/tx/FastRawTransactionManager.java
@@ -10,14 +10,14 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.tx;
+package org.tpc.tx;
 
 import java.io.IOException;
 import java.math.BigInteger;
 
-import org.web3j.crypto.Credentials;
-import org.web3j.protocol.Web3j;
-import org.web3j.tx.response.TransactionReceiptProcessor;
+import org.tpc.crypto.Credentials;
+import org.tpc.protocol.Web3j;
+import org.tpc.tx.response.TransactionReceiptProcessor;
 
 /**
  * Simple RawTransactionManager derivative that manages nonces to facilitate multiple transactions
