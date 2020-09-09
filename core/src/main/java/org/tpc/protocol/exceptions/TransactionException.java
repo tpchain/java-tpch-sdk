dbb97e87165d023d6be7566013c8a0ca7afb3e4e
zzx <slightingery@163.COM>
2020-09-09 09:21:14 +0800

    test

--- core/src/main/java/org/tpc/protocol/exceptions/TransactionException.java
+++ core/src/main/java/org/tpc/protocol/exceptions/TransactionException.java
@@ -10,11 +10,11 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.exceptions;
+package org.tpc.protocol.exceptions;
 
 import java.util.Optional;
 
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.tpc.protocol.core.methods.response.TransactionReceipt;
 
 /**
  * Transaction timeout exception indicates that we have breached some threshold waiting for a
