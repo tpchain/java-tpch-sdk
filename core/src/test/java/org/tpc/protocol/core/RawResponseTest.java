207ff49195855c0d26891a09ff7e3269206242d0
zzx <slightingery@163.COM>
2020-09-09 11:39:33 +0800

    test

--- core/src/test/java/org/tpc/protocol/core/RawResponseTest.java
+++ core/src/test/java/org/tpc/protocol/core/RawResponseTest.java
@@ -10,13 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core;
+package org.tpc.protocol.core;
 
 import org.junit.jupiter.api.Test;
 
-import org.web3j.protocol.ResponseTester;
-import org.web3j.protocol.core.methods.response.EthBlock;
-import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.tpc.protocol.ResponseTester;
+import org.tpc.protocol.core.methods.response.EthBlock;
+import org.tpc.protocol.core.methods.response.Web3ClientVersion;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
