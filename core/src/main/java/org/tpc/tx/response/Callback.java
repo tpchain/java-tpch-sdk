331e2f853658e4f2d076bd3d9017a37d496c4d8e
zzx <slightingery@163.COM>
2020-09-09 10:48:49 +0800

    test

--- core/src/main/java/org/tpc/tx/response/Callback.java
+++ core/src/main/java/org/tpc/tx/response/Callback.java
@@ -10,9 +10,9 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.tx.response;
+package org.tpc.tx.response;
 
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.tpc.protocol.core.methods.response.TransactionReceipt;
 
 /** Transaction receipt processor callback. */
 public interface Callback {
