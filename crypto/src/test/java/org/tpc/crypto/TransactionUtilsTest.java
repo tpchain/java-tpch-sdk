14e179da5634fb62e7d6079b719bebafb717341c
zzx <slightingery@163.COM>
2020-09-09 15:20:03 +0800

    test

--- crypto/src/test/java/org/tpc/crypto/TransactionUtilsTest.java
+++ crypto/src/test/java/org/tpc/crypto/TransactionUtilsTest.java
@@ -10,12 +10,12 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.web3j.crypto.TransactionUtils.generateTransactionHashHexEncoded;
+import static org.tpc.crypto.TransactionUtils.generateTransactionHashHexEncoded;
 
 public class TransactionUtilsTest {
 
