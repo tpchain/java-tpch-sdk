bf54686dc164cbb09a6a3155dbb308c525880b65
zzx <slightingery@163.COM>
2020-09-09 09:12:22 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/methods/response/Transaction.java
+++ core/src/main/java/org/tpc/protocol/core/methods/response/Transaction.java
@@ -10,11 +10,11 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.methods.response;
+package org.tpc.protocol.core.methods.response;
 
 import java.math.BigInteger;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 /** Transaction object used by both {@link EthTransaction} and {@link EthBlock}. */
 public class Transaction {
