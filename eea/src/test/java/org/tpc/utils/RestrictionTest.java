c9c8d981b47211eaaaea50a7c0d5b14ad2af48fd
zzx <slightingery@163.COM>
2020-09-09 15:51:01 +0800

    test

--- eea/src/test/java/org/tpc/utils/RestrictionTest.java
+++ eea/src/test/java/org/tpc/utils/RestrictionTest.java
@@ -10,13 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.utils;
+package org.tpc.utils;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.web3j.utils.Restriction.RESTRICTED;
-import static org.web3j.utils.Restriction.UNRESTRICTED;
+import static org.tpc.utils.Restriction.RESTRICTED;
+import static org.tpc.utils.Restriction.UNRESTRICTED;
 
 public class RestrictionTest {
 
