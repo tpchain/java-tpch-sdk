9c888624f1a97d6f38adb59a4afa4e991cfbbf3b
zzx <slightingery@163.COM>
2020-09-09 14:33:34 +0800

    test

--- core/src/test/java/org/tpc/utils/VersionTest.java
+++ core/src/test/java/org/tpc/utils/VersionTest.java
@@ -10,16 +10,16 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.utils;
+package org.tpc.utils;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.web3j.utils.Version.DEFAULT;
-import static org.web3j.utils.Version.getTimestamp;
-import static org.web3j.utils.Version.getVersion;
+import static org.tpc.utils.Version.DEFAULT;
+import static org.tpc.utils.Version.getTimestamp;
+import static org.tpc.utils.Version.getVersion;
 
 public class VersionTest {
 
