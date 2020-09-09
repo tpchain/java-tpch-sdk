eef09916259e8f3373cadd0411c445b0de3468a8
zzx <slightingery@163.COM>
2020-09-09 09:05:46 +0800

    test

--- core/src/main/java/org/tpc/protocol/core/methods/response/ShhMessages.java
+++ core/src/main/java/org/tpc/protocol/core/methods/response/ShhMessages.java
@@ -10,13 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.core.methods.response;
+package org.tpc.protocol.core.methods.response;
 
 import java.math.BigInteger;
 import java.util.List;
 
-import org.web3j.protocol.core.Response;
-import org.web3j.utils.Numeric;
+import org.tpc.protocol.core.Response;
+import org.tpc.utils.Numeric;
 
 /**
  * Whisper messages returned by:
