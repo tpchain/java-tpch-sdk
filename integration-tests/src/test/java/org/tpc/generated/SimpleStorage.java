24d4edaca016304b0652cbefb4b100d33c0f7838
zzx <slightingery@163.COM>
2020-09-09 17:28:29 +0800

    test

--- integration-tests/src/test/java/org/tpc/generated/SimpleStorage.java
+++ integration-tests/src/test/java/org/tpc/generated/SimpleStorage.java
@@ -1,25 +1,25 @@
-package org.web3j.generated;
+package org.tpc.generated;
 
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Collections;
-import org.web3j.abi.TypeReference;
-import org.web3j.abi.datatypes.Function;
-import org.web3j.abi.datatypes.Type;
-import org.web3j.abi.datatypes.generated.Uint256;
-import org.web3j.crypto.Credentials;
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.RemoteCall;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
-import org.web3j.tx.Contract;
-import org.web3j.tx.TransactionManager;
-import org.web3j.tx.gas.ContractGasProvider;
+import org.tpc.abi.TypeReference;
+import org.tpc.abi.datatypes.Function;
+import org.tpc.abi.datatypes.Type;
+import org.tpc.abi.datatypes.generated.Uint256;
+import org.tpc.crypto.Credentials;
+import org.tpc.protocol.Web3j;
+import org.tpc.protocol.core.RemoteCall;
+import org.tpc.protocol.core.methods.response.TransactionReceipt;
+import org.tpc.tx.Contract;
+import org.tpc.tx.TransactionManager;
+import org.tpc.tx.gas.ContractGasProvider;
 
 /**
  * <p>Auto generated code.
  * <p><strong>Do not modify!</strong>
  * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
- * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * or the org.tpc.codegen.SolidityFunctionWrapperGenerator in the
  * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
  *
  * <p>Generated with web3j version 4.0.1.
@@ -52,7 +52,7 @@ public class SimpleStorage extends Contract {
     public RemoteCall<TransactionReceipt> set(BigInteger x) {
         final Function function = new Function(
                 FUNC_SET, 
-                Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(x)), 
+                Arrays.<Type>asList(new org.tpc.abi.datatypes.generated.Uint256(x)),
                 Collections.<TypeReference<?>>emptyList());
         return executeRemoteCallTransaction(function);
     }
