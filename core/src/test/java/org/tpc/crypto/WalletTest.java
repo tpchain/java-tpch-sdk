cd071a4561527d63b7fc24a93abb1fd4e4449483
zzx <slightingery@163.COM>
2020-09-09 11:10:44 +0800

    test

--- core/src/test/java/org/tpc/crypto/WalletTest.java
+++ core/src/test/java/org/tpc/crypto/WalletTest.java
@@ -10,14 +10,14 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import java.io.IOException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
