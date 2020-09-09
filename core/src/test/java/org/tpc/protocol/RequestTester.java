cbd8a801c8473909e86cc130b6f74e6235eaba67
zzx <slightingery@163.COM>
2020-09-09 11:21:15 +0800

    test

--- core/src/test/java/org/tpc/protocol/RequestTester.java
+++ core/src/test/java/org/tpc/protocol/RequestTester.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol;
+package org.tpc.protocol;
 
 import java.io.IOException;
 
@@ -24,7 +24,7 @@ import okhttp3.ResponseBody;
 import okio.Buffer;
 import org.junit.jupiter.api.BeforeEach;
 
-import org.web3j.protocol.http.HttpService;
+import org.tpc.protocol.http.HttpService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
