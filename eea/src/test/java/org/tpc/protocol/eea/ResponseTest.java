c8146217d08fe1f8577b8692c6ddaa9e3879c95d
zzx <slightingery@163.COM>
2020-09-09 15:45:28 +0800

    test

--- eea/src/test/java/org/tpc/protocol/eea/ResponseTest.java
+++ eea/src/test/java/org/tpc/protocol/eea/ResponseTest.java
@@ -10,12 +10,12 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.eea;
+package org.tpc.protocol.eea;
 
 import org.junit.jupiter.api.Test;
 
-import org.web3j.protocol.ResponseTester;
-import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.tpc.protocol.ResponseTester;
+import org.tpc.protocol.core.methods.response.EthSendTransaction;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
