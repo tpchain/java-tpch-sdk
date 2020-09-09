005813afc92384b9243dc9fe2bc8bd32a2a6c15b
zzx <slightingery@163.COM>
2020-09-09 14:52:01 +0800

    test

--- crypto/src/main/java/org/tpc/crypto/SignatureDataOperations.java
+++ crypto/src/main/java/org/tpc/crypto/SignatureDataOperations.java
@@ -10,12 +10,12 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import java.math.BigInteger;
 import java.security.SignatureException;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 public interface SignatureDataOperations {
 
