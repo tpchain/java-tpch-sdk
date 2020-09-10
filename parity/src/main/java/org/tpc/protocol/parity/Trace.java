b3026e189a97514ceaea9f4e3ba903403c32db11
zzx <slightingery@163.COM>
2020-09-10 09:49:45 +0800

    test

--- parity/src/main/java/org/tpc/protocol/parity/Trace.java
+++ parity/src/main/java/org/tpc/protocol/parity/Trace.java
@@ -10,18 +10,18 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.parity;
+package org.tpc.protocol.parity;
 
 import java.math.BigInteger;
 import java.util.List;
 
-import org.web3j.protocol.core.DefaultBlockParameter;
-import org.web3j.protocol.core.Request;
-import org.web3j.protocol.core.methods.request.Transaction;
-import org.web3j.protocol.parity.methods.request.TraceFilter;
-import org.web3j.protocol.parity.methods.response.ParityFullTraceResponse;
-import org.web3j.protocol.parity.methods.response.ParityTraceGet;
-import org.web3j.protocol.parity.methods.response.ParityTracesResponse;
+import org.tpc.protocol.core.DefaultBlockParameter;
+import org.tpc.protocol.core.Request;
+import org.tpc.protocol.core.methods.request.Transaction;
+import org.tpc.protocol.parity.methods.request.TraceFilter;
+import org.tpc.protocol.parity.methods.response.ParityFullTraceResponse;
+import org.tpc.protocol.parity.methods.response.ParityTraceGet;
+import org.tpc.protocol.parity.methods.response.ParityTracesResponse;
 
 /** * JSON-RPC Parity traces API request object building factory. */
 public interface Trace {
