98e15e991b4926019e99d0dda829bdc68c9bbba5
zzx <slightingery@163.COM>
2020-09-10 11:15:45 +0800

    test

--- utils/src/test/java/org/tpc/utils/AssertionsTest.java
+++ utils/src/test/java/org/tpc/utils/AssertionsTest.java
@@ -10,12 +10,12 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.utils;
+package org.tpc.utils;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.web3j.utils.Assertions.verifyPrecondition;
+import static org.tpc.utils.Assertions.verifyPrecondition;
 
 public class AssertionsTest {
 
