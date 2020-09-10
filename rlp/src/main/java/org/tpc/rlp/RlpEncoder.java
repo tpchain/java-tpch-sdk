99d9951600b0adfde2132d31229ba1b861eed202
zzx <slightingery@163.COM>
2020-09-10 10:23:01 +0800

    test

--- rlp/src/main/java/org/tpc/rlp/RlpEncoder.java
+++ rlp/src/main/java/org/tpc/rlp/RlpEncoder.java
@@ -10,13 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.rlp;
+package org.tpc.rlp;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.web3j.rlp.RlpDecoder.OFFSET_SHORT_LIST;
-import static org.web3j.rlp.RlpDecoder.OFFSET_SHORT_STRING;
+import static org.tpc.rlp.RlpDecoder.OFFSET_SHORT_LIST;
+import static org.tpc.rlp.RlpDecoder.OFFSET_SHORT_STRING;
 
 /**
  * Recursive Length Prefix (RLP) encoder.
