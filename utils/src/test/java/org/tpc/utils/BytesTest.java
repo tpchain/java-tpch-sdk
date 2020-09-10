8a39ef24ef4eb956017f6a92463fd9423cd9aea3
zzx <slightingery@163.COM>
2020-09-10 11:16:36 +0800

    test

--- utils/src/test/java/org/tpc/utils/BytesTest.java
+++ utils/src/test/java/org/tpc/utils/BytesTest.java
@@ -10,12 +10,12 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.utils;
+package org.tpc.utils;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.web3j.utils.Bytes.trimLeadingZeroes;
+import static org.tpc.utils.Bytes.trimLeadingZeroes;
 
 public class BytesTest {
 
