07a759d0b7a53ac41c1dea0aa3af84dd309f15b2
zzx <slightingery@163.COM>
2020-09-10 11:13:38 +0800

    test

--- utils/src/test/java/org/tpc/crypto/HashTest.java
+++ utils/src/test/java/org/tpc/crypto/HashTest.java
@@ -10,13 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.crypto;
+package org.tpc.crypto;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.web3j.utils.Numeric.asByte;
+import static org.tpc.utils.Numeric.asByte;
 
 public class HashTest {
 
