328039e92d52a203816f2cdee554e8231956835a
zzx <slightingery@163.COM>
2020-09-10 09:58:48 +0800

    test

--- parity/src/main/java/org/tpc/protocol/parity/methods/response/ParityDeriveAddress.java
+++ parity/src/main/java/org/tpc/protocol/parity/methods/response/ParityDeriveAddress.java
@@ -10,9 +10,9 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.parity.methods.response;
+package org.tpc.protocol.parity.methods.response;
 
-import org.web3j.protocol.core.Response;
+import org.tpc.protocol.core.Response;
 
 /** parity_deriveAddressHash parity_deriveAddressIndex. */
 public class ParityDeriveAddress extends Response<String> {
