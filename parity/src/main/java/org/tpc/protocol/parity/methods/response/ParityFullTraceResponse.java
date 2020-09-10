5e2e8a0c09888a60276802cec895c14fe8d0c7b5
zzx <slightingery@163.COM>
2020-09-10 10:01:41 +0800

    test

--- parity/src/main/java/org/tpc/protocol/parity/methods/response/ParityFullTraceResponse.java
+++ parity/src/main/java/org/tpc/protocol/parity/methods/response/ParityFullTraceResponse.java
@@ -10,9 +10,9 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.protocol.parity.methods.response;
+package org.tpc.protocol.parity.methods.response;
 
-import org.web3j.protocol.core.Response;
+import org.tpc.protocol.core.Response;
 
 /** trace_rawTransaction trace_replayTransaction. */
 public class ParityFullTraceResponse extends Response<FullTraceInfo> {
