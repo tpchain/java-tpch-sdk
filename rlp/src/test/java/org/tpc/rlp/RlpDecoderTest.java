1ffa5805010d6f6ec54aaa4f64554b439f6058eb
zzx <slightingery@163.COM>
2020-09-10 10:28:44 +0800

    test

--- rlp/src/test/java/org/tpc/rlp/RlpDecoderTest.java
+++ rlp/src/test/java/org/tpc/rlp/RlpDecoderTest.java
@@ -10,13 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.rlp;
+package org.tpc.rlp;
 
 import java.math.BigInteger;
 
 import org.junit.jupiter.api.Test;
 
-import org.web3j.utils.Numeric;
+import org.tpc.utils.Numeric;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
